205f5f2069f02cec0a1c29adf7dfa08ab6d24ff9
alihassanrizvi1 <alihassanrizvi1@gmail.com>
2023-12-05 01:41:47 +0500

    ikonik task

---
+++ app/Services/MerchantService.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Services;
+
+use App\Jobs\PayoutOrderJob;
+use App\Models\Affiliate;
+use App\Models\Merchant;
+use App\Models\Order;
+use App\Models\User;
+
+class MerchantService
+{
+    /**
+     * Register a new user and associated merchant.
+     * Hint: Use the password field to store the API key.
+     * Hint: Be sure to set the correct user type according to the constants in the User model.
+     *
+     * @param array{domain: string, name: string, email: string, api_key: string} $data
+     * @return Merchant
+     */
+    public function register(array $data): Merchant
+    {
+        // TODO: Complete this method
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => $data['api_key'],
+            'type' => User::TYPE_MERCHANT,
+        ]);
+        $user->save();
+        $merchant = Merchant::create([
+            'domain' => $data['domain'],
+            'user_id'=> $user->id,
+        ]);
+        $merchant->save();
+    }
+
+    /**
+     * Update the user
+     *
+     * @param array{domain: string, name: string, email: string, api_key: string} $data
+     * @return void
+     */
+    public function updateMerchant(User $user, array $data)
+    {
+        // TODO: Complete this method
+        User::whereId($user->id)
+        ->update([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => $data['api_key'],
+        ]);
+        Merchant::where('user_id' , '=', $user->id)
+        ->update([
+            'domain' => $data['domain'],
+        ]);
+    }
+
+    /**
+     * Find a merchant by their email.
+     * Hint: You'll need to look up the user first.
+     *
+     * @param string $email
+     * @return Merchant|null
+     */
+    public function findMerchantByEmail(string $email): ?Merchant
+    {
+        // TODO: Complete this method
+        $merchant = Merchant::where('email' , '=', $email)->first();
+        return $merchant;
+    }
+
+    /**
+     * Pay out all of an affiliate's orders.
+     * Hint: You'll need to dispatch the job for each unpaid order.
+     *
+     * @param Affiliate $affiliate
+     * @return void
+     */
+    public function payout(Affiliate $affiliate)
+    {
+        // TODO: Complete this method
+        PayoutOrderJob::dispatch();
+    }
+}
